c25e7b8953438983b63c21df9cc710e4f348a50b
minhdinh <minhdinhthanh@vccorp.vn>
2022-10-10 08:26:46 +0700

    create project and connect mongo

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+// import Cate from './router/routercate'
+// import User from './router/routeruser'
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+try {
+  ;(async () => {
+    await mongoose.connect(
+        `mongodb+srv://admin:m123456@cluster0.lwode.mongodb.net/database_royalJourney?retryWrites=true&w=majority`
+    )
+    console.log('connected db ')
+  })()
+} catch (error) {
+  console.log('connected failed')
+}
+
+// app.use('/api', Cate)
+// app.use('/api', User)
+app.listen(process.env.PORT, () => {
+  console.log(`connected port ${process.env.PORT}`)
+})
