0da334331e5d89f44cfdca7160dcf5c86daf60fd
minhdinh <minhdinhthanh@vccorp.vn>
2022-10-11 08:26:01 +0700

    chang signin

--- src/controller/user/signIn.js
+++ src/controller/user/signIn.js
@@ -3,16 +3,28 @@ const bcyrpt = require('bcrypt')
 export const loginUser = async (req, res) => {
   try {
     const checkEmail = await user.findOne({ email: req.body.email })
-    if (!checkEmail) return res.status(404).json('tài khoản chưa đăng ký!')
+    if (!checkEmail) return res.status(404).json(
+      {
+        status: 'false',
+        message: "Email chưa được đăng kí",
+      }
+    )
     const checkPass = bcyrpt.compareSync(req.body.password, checkEmail.password)
-    if (!checkPass) return res.status(400).json('sai mật khẩu!')
+    if (!checkPass) return res.status(400).json({
+      status: 'false',
+      message: "Tài khoản hoặc mật khẩu không chính xác",
+    })
     res.status(200).json({
-      messege: 'true',
-      name: checkEmail.name,
-      email: checkEmail.email,
-      image: checkEmail.image,
-      phone: checkEmail.phone,
-      address: checkEmail.address,
+      status: 'true',
+      message: "Đăng nhập thành công",
+      data: {
+        id: checkEmail.id,
+        name: checkEmail.name,
+        email: checkEmail.email,
+        image: checkEmail.image,
+        phone: checkEmail.phone,
+        address: checkEmail.address,
+      }
     })
   } catch (error) {
     res.status(401).json({
