0c6dd83b48be2526c4d657a5267e1edb8385233a
minhdinh <minhdinhthanh@vccorp.vn>
2022-10-24 08:19:35 +0700

    get list product near by

--- src/router/routeruser.js
+++ src/router/routeruser.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { loginUser, loginHost, loginAdmin } from '../controller/user/signIn'
 import { createHost, createUser } from '../controller/user/signUp'
+import { nearByUserLocation } from '../controller/product/userLocationNearBy'
 const router = Router()
 router.route('/signup').post(createUser)
 router.route('/signin').post(loginUser)
 router.route('/host/signup').post(createHost)
 router.route('/host/signin').post(loginHost)
 router.route('/admin/signin').post(loginAdmin)
+router.route('/nearmylocation').get(nearByUserLocation)
 export default router
