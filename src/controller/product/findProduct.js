6212e4703b0d25ee8357b6647cceb6ccf3bbacb9
Admin <tailoc060899@gmail.com>
2022-11-25 21:08:42 +0700

    filter

--- src/controller/product/findProduct.js
+++ src/controller/product/findProduct.js
@@ -1,33 +1,59 @@
 import product from '../../models/product'
+import category from "../../models/category";
 
 export const getProduct = async (req, res) => {
-  const filter = { _id: req.params.id }
-  try {
-    const data = await product
-      .findById(filter)
-      .populate('user')
-      .populate({ path: 'category', model: 'Category', select: 'name' })
-    res.status(200).json(
-      data
-      )
-  } catch (error) {
-    res.status(400).json({
-      // error
-      message: error,
-    })
-  }
+    const filter = {_id: req.params.id}
+    try {
+        const data = await product
+            .findById(filter)
+            .populate('user')
+            .populate({path: 'category', model: 'Category', select: 'name'})
+        res.status(200).json(
+            data
+        )
+    } catch (error) {
+        res.status(400).json({
+            // error
+            message: error,
+        })
+    }
 }
 export const getProducts = async (req, res) => {
-  try {
-    const data = await product.find()
-    res.status(200).json({
-      message: 'true',
-      datapros: data,
-    })
-  } catch (error) {
-    res.status(400).json({
-      // error
-      message: 'false',
-    })
-  }
+    try {
+        const data = await product.find()
+        res.status(200).json({
+            message: 'true',
+            datapros: data,
+        })
+    } catch (error) {
+        res.status(400).json({
+            // error
+            message: 'false',
+        })
+    }
+}
+
+export const filterProducts = async (req, res) => {
+    try {
+        const idCategory =req.params.idCategory.split(',');
+        const data = await product.find({
+            price:{
+                $gte:Number(req.params.startPrice)+1,
+                $lt: Number(req.params.endPrice)+1
+            },
+            sao:{
+                $gte:Number(req.params.sao),
+                $lt: 6
+            },
+            category: Object(idCategory),
+        })
+        res.status(200).json({
+            message: 'true',
+            datapros: data,
+        })
+    } catch (error) {
+        res.status(400).json({
+            error
+        })
+    }
 }
--- src/router/routerProduct.js
+++ src/router/routerProduct.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { addProduct } from '../controller/product/addProduct'
 import { deleteProduct } from '../controller/product/deleteProduct'
-import { getProduct, getProducts } from '../controller/product/findProduct'
+import {filterProducts, getProduct, getProducts} from '../controller/product/findProduct'
 import {updateSaoProduct} from "../controller/product/updateProduct";
 
 const router = Router()
@@ -10,5 +10,6 @@ router.get('/listProduct/:id', getProduct)
 router.post('/addProduct', addProduct)
 router.delete('/deleteProduct/:id', deleteProduct)
 router.get('/updateSao/:id&:sao', updateSaoProduct)
+router.get('/listFilterProduct/:startPrice&:endPrice&:sao&:idCategory', filterProducts)
 
 export default router
